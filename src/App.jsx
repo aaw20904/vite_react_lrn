840de25ff489a59295fc7eef66015a983ca68d2f
Andrii Androsovych <kozakizdona@gmail.com>
2024-06-26 12:09:12 +0300

    dfgdgz

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { useState } from 'react'
 import * as bootstrap from 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
-import {MemeHeader,Meme } from './modules'
+ 
 import './App.css'
  
 
 function App() {
  
   
+
  return (
-   <ul className='container d-flex flex-column justify-content-start align-items-center w-100'>
-      <MemeHeader />
-      <Meme />
-   </ul>
+   <h1>{Date.now()}</h1>
   )
 }
 
